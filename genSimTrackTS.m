a725fbbafd7d5551b5421e04c9a07f61a1a9a389
Seamus Holden <seamus.holden@ncl.ac.uk>
2020-08-26 15:53:02 +0100

    bootstrapping tracks done
    bootstrap tracks for each simulated kymograph from the real data list of ~1000 tracks

---
+++ genSimTrackTS.m
@@ -0,0 +1,10 @@
+function trackBootstrapped=genSimTrackTS(trackTable,nTrackKymoAvg)
+%function trackBootstrapped=genSimTrackTS(trackTable,nTrackKymoAvg)
+% trackTable is a table of track cata, each row is a different track, each column 
+%   lists a track property including but not limited to lifetime and speed
+
+nTrackData=size(trackTable,1);
+nId=poissrnd(nTrackKymoAvg);
+bootId=randi(nTrackData,nId,1)
+trackBootstrapped=trackTable(bootId,:);
+
